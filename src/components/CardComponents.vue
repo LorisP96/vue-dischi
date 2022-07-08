9b572963815909c53d13c955a15c83c38a817308
LorisP96 <lorispalazzo96@gmail.com>
2022-07-08 16:49:57 +0200

    fine

---
+++ src/components/CardComponents.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <div class="card-mini album-image">
+      <img :src="SingleCard.poster" :alt="SingleCard.genre">
+    </div>
+    <div class="card-mini album-info">
+          <h2>{{ SingleCard.title }}</h2>
+          <div class="artist">{{ SingleCard.author }}</div>
+          <div class="year">{{ SingleCard.year }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CardComponents',
+  props: { 
+    SingleCard: Object,
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../assets/scss/style.scss';
+  .card {
+        width: calc((100% / 5) - 50px);
+        margin-bottom: 20px;
+        background-color: $main-color;
+        margin: 20px;
+        .card-mini {
+          width: 100%;
+          aspect-ratio: 1/1;
+        }
+        .album-image {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 20px;
+          img {
+            width: 100%;
+            object-fit: cover;
+          }
+        }
+        .album-info {
+          color: grey;
+          text-align: center;
+          padding: 10px;
+          h2 {
+            padding: 10px 0 20px;
+            color: white;
+          }
+          .artist {
+            font-size: 17px;
+          }
+          .year {
+            font-size: 14px;
+          }
+        }
+      }
+</style>
--- src/components/MainComponents.vue
+++ src/components/MainComponents.vue
@@ -1,22 +1,30 @@
 <template>
   <section>
     <div class="container">
-      <!-- <div class="card">
-        <div class="card-mini album-image">IMMAGINE</div>
-        <div class="card-mini album-info">
-          <h2>EZPZDFAS DFSDS ADS asd</h2>
-          <div class="artist">NOISY</div>
-          <div class="year">1996</div>
-        </div>
-      </div> -->
+      <CardComponents v-for="album, index in albumArray" :key="index" :SingleCard="album" />
     </div>
   </section>
 </template>
 
 <script>
+import CardComponents from '../components/CardComponents.vue';
+import axios from 'axios';
+
 export default {
-  name: 'MainComponents',
-}
+  name: "MainComponents",
+  components: { CardComponents },
+  data() {
+    return {
+      albumArray: [],
+    }
+  },
+  mounted() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((response) => {
+      this.albumArray = response.data.response;
+    })
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -25,44 +33,14 @@ export default {
   section {
     background-color: $bg-color;
     width: 100%;
-    height: calc(100vh - 70px);
     padding-top: 80px;
     .container {
       width: 70%;
       height: 100%;
       margin: 0 auto;
-      background-color: pink;
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
-      // .card {
-      //   width: calc((100% / 5) - 10px);
-      //   .card-mini {
-      //     background-color: coral;
-      //     width: 100%;
-      //     aspect-ratio: 1/1;
-      //   }
-      //   .album-image {
-      //     display: flex;
-      //     align-items: center;
-      //     justify-content: center;
-      //     padding: 20px;
-      //   }
-      //   .album-info {
-      //     color: grey;
-      //     text-align: center;
-      //     h2 {
-      //       padding: 10px 0 20px;
-      //       color: white;
-      //     }
-      //     .artist {
-      //       font-size: 17px;
-      //     }
-      //     .year {
-      //       font-size: 14px;
-      //     }
-      //   }
-      // }
     }
   }
 </style>

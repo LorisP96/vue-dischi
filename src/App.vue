785d83dff0e2036d81e247c3d8539a2bfe0ff709
LorisP96 <lorispalazzo96@gmail.com>
2022-07-08 15:08:58 +0200

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponents />
+    <main>
+      <MainComponents />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponents from './components/HeaderComponents.vue';
+import MainComponents from './components/MainComponents.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponents,
+    MainComponents
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/style.scss';
 </style>
---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponents',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '../assets/scss/style.scss';
+  header {
+    height: 70px;
+    background-color: $main-color;
+    display: flex;
+    align-items: center;
+    img {
+      width: 42px;
+      margin-left: 20px;
+    }
+  }
+</style>
